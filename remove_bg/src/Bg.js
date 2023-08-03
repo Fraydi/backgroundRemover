e930171d92742268e396e22936c759c8fdd913a0
Fraydi <fraydi123@gmail.com>
2023-08-03 10:43:54 +0300

    upload assets and start the code (1)

---
+++ remove_bg/src/Bg.js
@@ -0,0 +1,14 @@
+import './Bg.css';
+
+function Bg() {
+  return (
+    <div className = "Bg">
+        <div className = "header">
+            <span className = "header_span"> העלאת תמונה כדי להסיר את הרקע </span>
+            <button className = "uploadButtobn"> העלאת תמונה </button>
+        </div>
+    </div>
+  );
+}
+
+export default Bg;
